9cfd55f3610242655ebcf0ba3ab5d2bd81b12798
Deneme <yunus10atahan@gmail.com>
2019-03-13 10:23:56 +0300

    Added header and footer components

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <div class="menu">
+      <h3>This is menu</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+  
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
+    <Header></Header>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to OverLOG"/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
