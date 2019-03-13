604abb839b970ca6e772f11a698cbab42a13ea75
Deneme <yunus10atahan@gmail.com>
2019-03-13 20:55:24 +0300

    Cleanup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,12 @@
 <template>
-  <mdb-navbar class="flexible-navbar white" light position="bottom" scrolling>
-      <mdb-navbar-nav right>
-          <mdb-nav-item href="#" waves-fixed><mdb-icon fab class="text-black" icon="facebook-square"/></mdb-nav-item>
-          <mdb-nav-item href="#" waves-fixed><mdb-icon fab icon="twitter"/></mdb-nav-item>
-          <mdb-nav-item href="https://github.com/overlog" waves-fixed class="border border-light rounded mr-1" target="_blank"><mdb-icon fab icon="github" class="mr-2"/>OverLOG GitHub </mdb-nav-item>
-      </mdb-navbar-nav>
-  </mdb-navbar>
+  <div></div>
 </template>
 
 <script>
-import {mdbNavbar, mdbNavItem, mdbNavbarNav, mdbIcon} from 'mdbvue'
 
 export default {
   name: 'Header',
-  components: {
-    mdbNavbar,
-    mdbNavItem,
-    mdbNavbarNav,
-    mdbIcon
-  }
+
 }
 </script>
 
