33e5e5a32d9513cd71c976145c574602e28a9671
Deneme <yunus10atahan@gmail.com>
2019-03-16 21:15:44 +0300

    Axios post, get example added.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,27 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import axios from 'axios';
+
+//const API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+//const data = axios.get(API_URL).then(response => response.data);
+
+axios.get('https://reqres.in/api/users?page=2').then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.log(error);
+});
+
+/*axios.post('http://localhost/test/login.php', {
+    username: 'test',
+    password: 'user@test'
+  })
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });*/
 
 export default {
   name: 'home',
