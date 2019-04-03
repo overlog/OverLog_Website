b04fea1e47290687c4da635710881f8f8df8b395
Yunus Atahan <yunus10atahan@gmail.com>
2019-04-03 21:04:34 +0300

    Refactor backend and change port to 8081

--- server/app.py
+++ server/app.py
@@ -1,22 +1,42 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
-
-# configuration
-DEBUG = True
-
-# instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-# enable CORS
 CORS(app)
+db = SQLAlchemy(app)
 
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+# Database
+engine = create_engine('postgresql://postgres:12345@localhost:5432/overlog')
+
+
+
+
+
+@app.route('/')
+def hello_world():
+
+
+    print("asdasd")
+    connection = engine.connect()
+    query = ("select * from log")
+    result = connection.execute(query)
+    dict = {}
+    for i in result:
+        dict[i["id"]] = {"type": i["type"]}
+
+    print(dict)
+
+    return jsonify(dict)
+
+
+@app.route("/veri", methods = ["GET"])
+def get():
+    return "asdasda"
+
+
 
 
 if __name__ == '__main__':
