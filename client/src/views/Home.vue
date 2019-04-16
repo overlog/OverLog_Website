29ba82133cadd42839496006dae552175ef3f7f2
Yunus Atahan <yunus10atahan@gmail.com>
2019-04-16 22:53:30 +0300

    Correct error

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -29,28 +29,11 @@
   });*/
 
 export default {
-<<<<<<< HEAD
-  name: "home"
-=======
   name: "home",
->>>>>>> c2950c9ec3acf2d6322f061d8e83d2c76b782990
 };
 </script>
 
 <style scoped>
-<<<<<<< HEAD
-body {
-  width: 100%;
-  height: 100%;
-}
-html {
-  height: 100%;
-}
-.bg {
-  /* The image used */
-  background-image: url("http://sfwallpaper.com/images/hd-wallpaper-1080p-widescreen-free-6.jpg");
-=======
->>>>>>> c2950c9ec3acf2d6322f061d8e83d2c76b782990
 
 </style>
 
--- server/app.py
+++ server/app.py
@@ -9,7 +9,8 @@ db = SQLAlchemy(app)
 
 
 # Database
-engine = create_engine('postgresql://postgres:12345@localhost:5432/overlog')
+#engine = create_engine('postgresql://postgres:12345@localhost:5432/overlog')
+engine = create_engine('postgres://wwignxrncfkuoj:42995cfd99e0913fe7385a6237401e81bd88c735c2285cb590f8c1a874a732bf@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d11mskt5lrs57u')
 
 
 
