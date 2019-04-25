1ca168387a5c780cae71a2d427aa5050fe7d7941
konik <konik@konik-W350SKQ-W370SK.(none)>
2019-04-25 21:00:01 +0300

    düzeltme

--- client/src/store.js
+++ client/src/store.js
@@ -12,17 +12,5 @@ export default new Vuex.Store({
   },
   actions: {
 
-<<<<<<< HEAD
-      fetch('localhost:5000/')
-      .then((response) => {
-        console.log(response);
-
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    }
-=======
->>>>>>> b01db4b1c2e7fccb056d5e2e2a8becedaf499594
   }
 })
